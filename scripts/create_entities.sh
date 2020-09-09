da74f011474934f29e279babc57aab5522714ac3
Shimi Bandiel <shimib@jfrog.com>
2020-09-09 09:36:17 -0700

    debug

--- scripts/create_entities.sh
+++ scripts/create_entities.sh
@@ -71,7 +71,7 @@ echo $JPD_VALUES
 TARGET_JPDS=`echo $JPD_VALUES | jq -c  '. | map (. | select (.id != "JPD-1"))'`
 echo $TARGET_JPDS
 echo "XXXXX"
-echo "{\\"entities\\" : [\\"USERS\\",\\"GROUPS\\",\\"PERMISSIONS\\",\\"TOKENS\\"], \\"targets\\" : ${TARGET_JPDS} }" 
+echo '{\\"entities\\" : [\\"USERS\\",\\"GROUPS\\",\\"PERMISSIONS\\",\\"TOKENS\\"], \\"targets\\" : ${TARGET_JPDS} }' 
 echo "XXXXX"
 curl -X PUT -d "{\\"entities\\" : [\\"USERS\\",\\"GROUPS\\",\\"PERMISSIONS\\",\\"TOKENS\\"], \\"targets\\" : ${TARGET_JPDS} }" -H "Content-Type:application/json" -H "Authorization: Bearer ${MC_TOKEN}" ${BASEURL}/mc/api/v1/federation/JPD-1
 ######################
